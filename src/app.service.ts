767c5be1845e01604d4adf829b57fd478889ca9c
CarlosMrtnzMrtnz <carlosmartinezmartinez2015@gmail.com>
2024-06-11 10:52:35 -0500

    Creacion de branch 'soltinez'

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return '🎊🥳🎊🥳 Welcome to this Challenge 2,024...!!! 🥳🎊🥳🎊';
   }
+
+  getPrivate(): string {
+    return 'This is a protected resource. Register now visitor!';
+  }
 }
---
+++ src/authorization/authorization.guard.ts
@@ -0,0 +1,40 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { expressJwtSecret } from 'jwks-rsa';
+import { promisify } from 'util';
+import { expressjwt } from 'express-jwt';
+
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class AuthorizationGuard implements CanActivate {
+
+  private CLIENT_ID: string = process.env.CLIENT_ID
+  private ISSUER_BASE_URL: string = process.env.ISSUER_BASE_URL
+
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+
+      const req = context.getArgByIndex(0)
+      const res = context.getArgByIndex(1)
+
+      const checkJwt = promisify (
+        expressjwt({
+          secret: expressJwtSecret({
+            cache: true,
+            rateLimit: true,
+            jwksRequestsPerMinute: 5,
+            jwksUri: `${this.ISSUER_BASE_URL}.well-known/jwks.json`
+          })as any,
+          audience: this.CLIENT_ID,
+          issuer: this.ISSUER_BASE_URL,
+          algorithms: ['RS256']
+        })
+      );
+      try {
+        await checkJwt(req, res)
+        return true;
+      } catch (error) {
+        throw new UnauthorizedException(error)
+      }
+  }
+}
