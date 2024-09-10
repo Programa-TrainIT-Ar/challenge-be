78419cb895a6aab4966f32417ab007352b5a8f18
charinwolf <rcharinwolf@gmail.com>
2024-09-10 16:34:13 -0300

    Creadas las tablas Question, QuestionCell y SkillLevel

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, SetMetadata, UseGuards } from '@nestjs/common';
+git import { Controller, Get, SetMetadata, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthorizationGuard } from './authorization/authorization.guard';
 
