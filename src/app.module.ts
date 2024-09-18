73569058a1e148e79a9f14260f735acb1beb3f2f
jesusdanielGIT <kosku5000@gmail.com>
2024-09-17 22:07:37 -0500

    configuracion de seguridad

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma-module/prisma.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
+import { AuthorizationModule } from './authorization/authorization.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, UsuariosModule],
+  imports: [PrismaModule, UsuariosModule, AuthorizationModule,ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/authorization/authorization.guard.ts
@@ -0,0 +1,48 @@
+import { CanActivate, ExecutionContext, Injectable, Redirect, UnauthorizedException } from '@nestjs/common';
+import { expressJwtSecret } from 'jwks-rsa';
+import { promisify } from 'util';
+import { expressjwt } from 'express-jwt';
+import * as jwt from 'express-jwt'
+import { log } from 'console';
+
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class AuthorizationGuard implements CanActivate {
+
+  private CLIENT_ID: string = process.env.CLIENT_ID
+  private ISSUER_BASE_URL: string = process.env.ISSUER_BASE_URL
+
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+
+      const req = context.getArgByIndex(0)
+      const res = context.getArgByIndex(1)
+
+      
+
+      const checkJwt = promisify (
+        expressjwt({
+          secret: expressJwtSecret({
+            cache: true,
+            rateLimit: true,
+            jwksRequestsPerMinute: 5,
+            jwksUri: `${this.ISSUER_BASE_URL}.well-know/jwks.json`
+          })as any,
+          audience: this.CLIENT_ID,
+          issuer: this.ISSUER_BASE_URL,
+          algorithms: ['RS256']
+        })
+      );
+      try {
+        await checkJwt(req, res)
+        return true;
+      } catch (error) {
+        console.log(this.ISSUER_BASE_URL);
+        throw new UnauthorizedException(error)
+
+        
+      }
+
+  }
+}
