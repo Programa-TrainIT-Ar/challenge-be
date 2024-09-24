2e6eda50a3ef1af07c498a47b146ddf640d527f9
jesusdanielGIT <kosku5000@gmail.com>
2024-09-23 19:06:03 -0500

    update user-relations

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,10 @@ import { PrismaModule } from './prisma-module/prisma.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { AuthorizationModule } from './authorization/authorization.module';
 import { ConfigModule } from '@nestjs/config';
-import { CandidatesModule } from './candidates/candidates.module';
+
 
 @Module({
-  imports: [PrismaModule,CandidatesModule, UsuariosModule, AuthorizationModule,ConfigModule.forRoot()],
+  imports: [PrismaModule, UsuariosModule, AuthorizationModule,ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
