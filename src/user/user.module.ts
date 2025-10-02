fc14a53179050d99810a183aee6ff42b2a6bce28
JSolorzano <jsolorzanosc@gmail.com>
2025-10-02 16:56:53 -0400

    feat: implementando local auth para el accountSetup

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,10 +4,26 @@ import { UserController } from './user.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { EmailService } from './email.service';
 import { HttpModule } from '@nestjs/axios'; // Importa el servicio HTTP para realizar peticiones externas
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [UserController],
   providers: [UserService, EmailService],
-  imports: [PrismaModule, HttpModule]
+  imports: [
+    PrismaModule,
+    HttpModule,
+    JwtModule.registerAsync({
+      //Configurando el JwtModule
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        //Obtener el valor de la variable JWT_SECRET de forma segura
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '3d' }, //Tiempo para expirar de cada token
+      }),
+      inject: [ConfigService], 
+    }),
+    
+  ], exports: [JwtModule]
 })
 export class UserModule {}
